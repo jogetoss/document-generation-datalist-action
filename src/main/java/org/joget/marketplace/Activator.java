a235c027b06542253e12d522bb906b561b499542
nabila861 <jahannabila690@gmail.com>
2025-07-21 11:40:49 +0600

    updated version

--- src/main/java/org/joget/marketplace/Activator.java
+++ src/main/java/org/joget/marketplace/Activator.java
@@ -8,7 +8,7 @@ import org.osgi.framework.ServiceRegistration;
 
 public class Activator implements BundleActivator {
 
-    public static final String VERSION = "8.0.10";
+    public static final String VERSION = "8.0.11";
 
     protected Collection<ServiceRegistration> registrationList;
 
